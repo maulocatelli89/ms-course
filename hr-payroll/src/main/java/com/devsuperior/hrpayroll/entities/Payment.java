7fdb2b3a34986bba01c00721bc3f0b83d991e825
Mauricio Locatelli <mauricio_locatelli@hotmail.com>
2021-09-03 11:41:02 +0100

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,50 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private Double dailyIncome;
+	private int days;
+	
+	public Payment() {
+	}
+	
+	public Payment(String name, Double dailyIncome, int days) {
+		super();
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+		this.days = days;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	public int getDays() {
+		return days;
+	}
+
+	public void setDays(int days) {
+		this.days = days;
+	}
+	
+	public double getTotal() {
+		return days * dailyIncome;
+	}
+}
